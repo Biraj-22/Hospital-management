529dcfb41a25ffde63f76be6ccb821f0dc82a1ec
Biraj-22 <63511436+Biraj-22@users.noreply.github.com>
2023-06-19 18:05:37 +0600

    Add files via upload

---
+++ Hospital-Management-website/-doctor-portal-Client-side-main/README.md
@@ -0,0 +1,3 @@
+// Live Site:  https://log-ino.web.app!
+
+![React App](https://user-images.githubusercontent.com/74599998/172770590-c156de7f-5d2e-47ed-ae6a-b7efaef87916.png)
---
+++ Hospital-Management-website/-doctor-portal-Client-side-main/src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRwNSRiWnFLeD9StylLhDqOFsPSiA6cy0",
+  authDomain: "log-ino.firebaseapp.com",
+  projectId: "log-ino",
+  storageBucket: "log-ino.appspot.com",
+  messagingSenderId: "249015987553",
+  appId: "1:249015987553:web:2d3a14e984b9fa761b2920"
+};
+
+export default firebaseConfig;
